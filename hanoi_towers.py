2812aedcb5639db359896655a4389dc81e12eac1
isaac <isaac>
2021-08-01 00:41:06 -0600

    Initial commit

---
+++ hanoi_towers.py
@@ -0,0 +1,19 @@
+
+def generate_moves_list(n):
+    def make_moves(n, origin, auxiliar, target):
+        # save.append((origin.copy(),  target.copy(), auxiliar.copy(), 'bottom2'))
+        if n == 1:
+            target.append(origin.pop())
+            save.append((rod_1.copy(), rod_2.copy(), rod_3.copy()))
+
+        if n > 1:
+            make_moves(n-1, origin, target, auxiliar)
+            target.append(origin.pop())
+            save.append((rod_1.copy(), rod_2.copy(), rod_3.copy()))
+            make_moves(n-1, auxiliar, origin, target)
+        return save
+
+    rod_1, rod_2, rod_3 = [i for i in range(n, 0, -1)], [], []
+    save = [(rod_1.copy(), rod_2.copy(), rod_3.copy())]
+    make_moves(n, rod_1, rod_2, rod_3)
+    return save
